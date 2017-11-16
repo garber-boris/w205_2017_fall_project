93607c8e661cf1eae7f31f1871cdd211e9f8aa98
garber-boris <garber-boris@users.noreply.github.com>
2017-11-16 18:39:45 -0500

    update to load file to remove header

--- load_data_lake.sh
+++ load_data_lake.sh
@@ -7,24 +7,24 @@ MY_CWD=$(pwd)
 hdfs dfs -mkdir /user/w205/project
 
 FILE="routes.txt"
-tail -n +1 "$FILE" > $NEW_FILE
+tail -n +1 "$FILE" > $FILE
 hdfs dfs -mkdir /user/w205/project/routes
-hdfs dfs -put $NEW_FILE /user/w205/project/routes
+hdfs dfs -put $FILE /user/w205/project/routes
 
 FILE="stops.txt"
-tail -n +1 "$FILE" > $NEW_FILE
+tail -n +1 "$FILE" > $FILE
 hdfs dfs -mkdir /user/w205/project/stops
-hdfs dfs -put $NEW_FILE /user/w205/project/stops
+hdfs dfs -put $FILE /user/w205/project/stops
 
 FILE="stop_times.txt"
-tail -n +1 "$FILE" > $NEW_FILE
+tail -n +1 "$FILE" > $FILE
 hdfs dfs -mkdir /user/w205/project/stop_times
-hdfs dfs -put $NEW_FILE /user/w205/project/stop_times
+hdfs dfs -put $FILE /user/w205/project/stop_times
 
 FILE="trips.txt"
-tail -n +1 "$FILE" > $NEW_FILE
+tail -n +1 "$FILE" > $FILE
 hdfs dfs -mkdir /user/w205/project/trips
-hdfs dfs -put $NEW_FILE /user/w205/project/trips
+hdfs dfs -put $FILE /user/w205/project/trips
 
 # change back to original directory
 cd $MY_CWD

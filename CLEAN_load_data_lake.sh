fe601c893170dc956660d16b627a7b451775c881
garber-boris <garber-boris@users.noreply.github.com>
2017-11-16 19:02:29 -0500

    update to load and clean files

--- CLEAN_load_data_lake.sh
+++ CLEAN_load_data_lake.sh
@@ -4,16 +4,16 @@
 MY_CWD=$(pwd)
 
 # remove files from hdfs
-hdfs dfs -rm /user/w205/project/routes/routes.txt
-hdfs dfs -rm /user/w205/project/stops/stops.txt
-hdfs dfs -rm /user/w205/project/stop_times/stop_times.txt
-hdfs dfs -rm /user/w205/project/trips/trips.txt
+hdfs dfs -rm /user/w205/project/routes/routes_clean.txt
+hdfs dfs -rm /user/w205/project/stops/stops_clean.txt
+hdfs dfs -rm /user/w205/project/stop_times/stop_times_clean.txt
+hdfs dfs -rm /user/w205/project/trips/trips_clean.txt
 
 # remove hdfs directory
-hdfs dfs -rmdir /user/w205/project/routes
-hdfs dfs -rmdir /user/w205/project/stops
-hdfs dfs -rmdir /user/w205/project/stop_times
-hdfs dfs -rmdir /user/w205/project/trips
+hdfs dfs -rmdir /user/w205/project/routes_clean
+hdfs dfs -rmdir /user/w205/project/stops_clean
+hdfs dfs -rmdir /user/w205/project/stop_times_clean
+hdfs dfs -rmdir /user/w205/project/trips_clean
 hdfs dfs -rmdir /user/w205/project
 
 # change back to original directory
--- load_data_lake.sh
+++ load_data_lake.sh
@@ -9,26 +9,26 @@ hdfs dfs -mkdir /user/w205/project
 OLD_FILE="routes.txt"
 FILE="routes_clean.txt"
 tail -n +2 "$OLD_FILE" > $FILE
-hdfs dfs -mkdir /user/w205/project/routes
-hdfs dfs -put $FILE /user/w205/project/routes
+hdfs dfs -mkdir /user/w205/project/routes_clean
+hdfs dfs -put $FILE /user/w205/project/routes_clean
 
 OLD_FILE="stops.txt"
 FILE="stops_clean.txt"
 tail -n +2 "$OLD_FILE" > $FILE
-hdfs dfs -mkdir /user/w205/project/stops
-hdfs dfs -put $FILE /user/w205/project/stops
+hdfs dfs -mkdir /user/w205/project/stops_clean
+hdfs dfs -put $FILE /user/w205/project/stops_clean
 
 OLD_FILE="stop_times.txt"
 FILE="stop_times_clean.txt"
 tail -n +2 "$OLD_FILE" > $FILE
-hdfs dfs -mkdir /user/w205/project/stop_times
-hdfs dfs -put $FILE /user/w205/project/stop_times
+hdfs dfs -mkdir /user/w205/project/stop_times_clean
+hdfs dfs -put $FILE /user/w205/project/stop_times_clean
 
 OLD_FILE="trips.txt"
 FILE="trips_clean.txt"
 tail -n +2 "$OLD_FILE" > $FILE
-hdfs dfs -mkdir /user/w205/project/trips
-hdfs dfs -put $FILE /user/w205/project/trips
+hdfs dfs -mkdir /user/w205/project/trips_clean
+hdfs dfs -put $FILE /user/w205/project/trips_clean
 
 # change back to original directory
 cd $MY_CWD

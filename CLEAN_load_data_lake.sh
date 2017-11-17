6a82ad444160d0b41bd854280e12f37dd0296e56
garber-boris <garber-boris@users.noreply.github.com>
2017-11-16 19:06:33 -0500

    update to clean file

--- CLEAN_load_data_lake.sh
+++ CLEAN_load_data_lake.sh
@@ -4,10 +4,10 @@
 MY_CWD=$(pwd)
 
 # remove files from hdfs
-hdfs dfs -rm /user/w205/project/routes/routes_clean.txt
-hdfs dfs -rm /user/w205/project/stops/stops_clean.txt
-hdfs dfs -rm /user/w205/project/stop_times/stop_times_clean.txt
-hdfs dfs -rm /user/w205/project/trips/trips_clean.txt
+hdfs dfs -rm /user/w205/project/routes_clean/routes_clean.txt
+hdfs dfs -rm /user/w205/project/stops_clean/stops_clean.txt
+hdfs dfs -rm /user/w205/project/stop_times_clean/stop_times_clean.txt
+hdfs dfs -rm /user/w205/project/trips_clean/trips_clean.txt
 
 # remove hdfs directory
 hdfs dfs -rmdir /user/w205/project/routes_clean
